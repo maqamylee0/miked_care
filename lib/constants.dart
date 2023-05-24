88ae5a071bd5a5dfe82df2fb072760b2a5b229d2
maqamylee0 <maqamylee0@gmail.com>
2023-05-24 21:28:21 +0300

    rm -sec

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,7 @@
 class Statics {
   static const String appId = "4dc47ab04ec442c6ba7f59f6745ce6bb";
-  // static const int appId = 67857598;
+  static const String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI3MTIyZDJlMi0wZjYzLTQyY2UtYmIxYi05YzdhNzcyYTNiZGYiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTY4NDkxODg0NSwiZXhwIjoxNzE2NDU0ODQ1fQ.DZ9um64Es1IPemnDL-hehasKBqQHv4uRhSPvPm0pe0M";
+
+// static const int appId = 67857598;
   // static const String appSign = "124d5670d800f11f7c4052ac29517ccc93df399136992af7ea3fe4b6805396ce";
 }
